2970e53db1907cc30afba088060fe7dd530542bb
Jacob <kapsjacob@gmail.com>
2019-01-10 01:49:32 +0530

    jagged array

--- README.md
+++ README.md
@@ -7,15 +7,6 @@ This repository contains all the java programming assignments for the CSE1007 la
 2. WAP, which takes two integer operands and one operator from the user, performs the operation and then prints the result. (Consider the operators `+, - ,*, /, %` and use `switch` Statement)
 3. WAP to find the sum of individual digits of a positive integer and test given number is palindrome.
 4. WAP to generate all the prime numbers between 1 and n, where n is a value supplied by the user.
-5. Write a program that calls a function `gcd(m,n)` which returns the greatest common divisor of the two values m and n.
-6. WAP to find the minimum and maximum integer in a list of integers. (Use array)
-7. WAP that uses functions to perform the following based on the user’s choice: (Use 2d array)
-    * Addition of Two Matrices
-    * Multiplication of Two Matrices 
-8. WAP that uses functions to perform the following operations: (Use functions)
-    * To insert a substring in to a given main string from a given position.
-    * To delete n Characters from a given position in a given string.
-9. WAP to determine if the given string is a palindrome or not.
 
 ## Assignment 2
 1. Write a program to demonstrate the knowledge of students in multidimensional arrays and looping constructs.
--- assignment2/src/assignment2/multiDim.java
+++ assignment2/src/assignment2/multiDim.java
@@ -1,10 +1,43 @@
 package assignment2;
+import java.util.Scanner;
 
 public class multiDim {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int i, j;
+        double t;
 
-	}
+		// Declaring 2-D array with 4 rows 
+        int arr[][] = new int[4][]; 
+  
+        // input for each batch
+        Scanner sc = new Scanner(System.in);
+        for(i = 0; i < arr.length; i++){
+            System.out.print("Enter number of students for batch" + (i+1) + ": ");
+            t = sc.nextDouble();
+            arr[i] = new int[(int)Math.ceil(t/4)];
+            for(j = 0; j < arr[i].length; j++)
+            {
+                if(t >= 4)
+                    arr[i][j] = 4;
+                else
+                    arr[i][j] = (int)t;
+                t = t - 4;
+            }
+        }
+  
+        // Displaying the values of 2D Jagged array 
+        int cfour = 0;
+        System.out.println("Contents of 2D Jagged Array"); 
+        for (i = 0; i < arr.length; i++) { 
+            for (j = 0; j < arr[i].length; j++) {
+            		System.out.print(arr[i][j] + " ");
+            		if(arr[i][j] == 4)
+            			cfour++;
+            }
+            System.out.println(); 
+        }
 
+        System.out.println("Number of tutors with 4 students are: " + cfour );
+    }
 }
