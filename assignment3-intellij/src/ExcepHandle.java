d8eb7c972fca6ee9e390b3ddf70dacccefe84e55
Jacob <kapsjacob@gmail.com>
2019-02-06 09:30:09 +0530

    exception handling

---
+++ assignment3-intellij/src/ExcepHandle.java
@@ -0,0 +1,42 @@
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExcepHandle{
+
+    static void validate(String r, String n){
+        if(r.length() != 9){
+            System.out.println("Invalid");
+            throw new IllegalArgumentException("Register Number does not contain exactly 9 characters");
+        }
+        if(n.length() != 10){
+            System.out.println("Invalid");
+            throw new IllegalArgumentException("Mobile Number does not contain exactly 10 characters");
+        }
+
+        String pattern = "^[6|7|8|9]{1}\\d{9}";
+        Pattern a = Pattern.compile(pattern);
+        Matcher m1 = a.matcher(n);
+        if(!m1.find()){
+            throw new NumberFormatException("Mobile Number cannot contain any character other than a digit");
+        }
+
+        String pattern2 = "^[1-9]{2}[A-Z]{3}[0-9]{4}$";
+        Pattern b = Pattern.compile(pattern2);
+        Matcher m2 = b.matcher(r);
+        if(!m2.find()){
+            throw new NoSuchElementException("Registration Number cannot contain any character other than digits and alphabets");
+        }
+
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String reg = sc.nextLine();
+        String no = sc.nextLine();
+
+        validate(reg, no);
+        System.out.println("Valid");
+    }
+}
