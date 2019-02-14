2bc8b34eee143fbcc88a607d3831d6bed8bdd730
Varun Reddy <varunreddy1999@outlook.com>
2019-02-14 11:47:06 +0530

    Updated Donor Program

--- assignment4/src/Donor.java
+++ assignment4/src/Donor.java
@@ -4,13 +4,24 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Donor {
-    String name,addr,contact,bldgrp,date;
+    String name,addr,contact,bldgrp;
+    Date date;
 
     public static void main(String[] args) throws IOException{
         int n, i;
+        SimpleDateFormat ft = new SimpleDateFormat("MM-dd-yyyy");
+        String temp;
+        String pattern = "[A|B|O|AB][+|-]";
+        Matcher m;
+        Pattern r = Pattern.compile(pattern);
 
         // delete existing file first
         try{
@@ -44,29 +55,51 @@ public class Donor {
             arr[i].addr = sc.nextLine();
             System.out.print("Contact: ");
             arr[i].contact = sc.nextLine();
-            System.out.print("Blood Group: ");
-            arr[i].bldgrp = sc.nextLine();
-            System.out.print("Date: ");
-            arr[i].date = sc.nextLine();
+            arr[i].bldgrp = "";
+            m = r.matcher(arr[i].bldgrp);
+            while(!m.find()){
+                System.out.print("Blood Group: ");
+                arr[i].bldgrp = sc.nextLine();
+                m = r.matcher(arr[i].bldgrp);
+            }
+            boolean flag = false;
+            while(!flag){
+                System.out.print("Date: ");
+                temp = sc.nextLine();
+                try {
+                    arr[i].date = ft.parse(temp);
+                    flag = true;
+                } catch (ParseException e) {
+                    flag = false;
+                    System.out.println("Unparseable using " + ft);
+                }
+            }
+
+
             // concatenating all properties in donor object
             // and converting to a byte stream
+            outputstream.write(("Donor " + (i+1) + "\n").getBytes());
+            outputstream.write("----------------\n".getBytes());
+            outputstream.write("Name: ".getBytes());
             outputstream.write(arr[i].name.getBytes());
             outputstream.write("\n".getBytes());
+            outputstream.write("Address: ".getBytes());
             outputstream.write(arr[i].addr.getBytes());
             outputstream.write("\n".getBytes());
+            outputstream.write("Contact: ".getBytes());
             outputstream.write(arr[i].contact.getBytes());
             outputstream.write("\n".getBytes());
+            outputstream.write("Blood Group: ".getBytes());
             outputstream.write(arr[i].bldgrp.getBytes());
             outputstream.write("\n".getBytes());
-            outputstream.write(arr[i].date.getBytes());
+            outputstream.write("Date: ".getBytes());
+            outputstream.write(arr[i].date.toString().getBytes());
             outputstream.write("\n".getBytes());
             outputstream.write("\n".getBytes());
-
-            // create a byte array to be written
-            byte c[] = outputstream.toByteArray();
-            fos.write(c);                                          // writing byte array to file
-            outputstream.flush();
         }
+        // create a byte array to be written
+        byte c[] = outputstream.toByteArray();
+        fos.write(c);                                          // writing byte array to file
         fos.close();
     }
 }
