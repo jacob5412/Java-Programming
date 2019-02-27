4c573e6dd0d598e012c1a95823b2b55596ab6102
Jacob <kapsjacob@gmail.com>
2019-02-27 09:27:35 +0530

    final assessment4

--- assignment4/src/HashStudents.java
+++ assignment4/src/HashStudents.java
@@ -2,18 +2,15 @@ import java.util.*;
 
 public class HashStudents {
     public static void main(String[] args) {
-        HashMap<String, List<String>> h1 = new HashMap<>();
-        HashMap<String, String> h2 = new HashMap<>();
+        HashMap<String, List<String>> h1 = new LinkedHashMap<>();
+        HashMap<String, String> h2 = new LinkedHashMap<>();
 
         // Check if HashMap is empty
         System.out.println("is studentsMapping empty?: " + h1.isEmpty());
         System.out.println("is studentsMapping empty?: " + h2.isEmpty());
 
-        // creating an ArrayList
-        ArrayList<String> al = new ArrayList<String>();
-
         // creating list of subjects and adding to hash map
-        List<String> subjects = Arrays.asList( "Python", "Math", "C");
+        List<String> subjects = Arrays.asList("Python", "Math", "C");
         h1.put("A",subjects);
         subjects = Arrays.asList("C","C++");
         h1.put("B",subjects);
@@ -34,6 +31,19 @@ public class HashStudents {
         for(Map.Entry m:h2.entrySet()){
             System.out.println(m.getKey()+" "+m.getValue());
         }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a student: ");
+        String s = sc.nextLine();
+
+        System.out.println("Faculties are: ");
+        h1.forEach((k, v) -> {
+            if(k.equals(s))
+            v.forEach(w -> {for(Map.Entry m:h2.entrySet()){
+                if(m.getKey().equals(w))
+                    System.out.println(m.getValue());
+            }});
+        });
     }
 }
 
