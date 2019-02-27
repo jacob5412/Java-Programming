2d1f6815e67208c4c159a45e9358976435f23e77
Jacob <kapsjacob@gmail.com>
2019-02-27 08:59:53 +0530

    added cat 2

---
+++ assignment4/src/MyCalculator.java
@@ -0,0 +1,36 @@
+/*
+ * Complete the function power in class MyCalculator and return the appropriate result
+ * after the power operation or an appropriate exception as detailed above.
+ */
+import java.util.Scanner;
+
+public class MyCalculator{
+    static long power(int n, int p) throws Exception{
+        if(n == 0 && p == 0){
+            throw new Exception("n and p should not be zero");
+        }
+        else if(n < 0 || p < 0){
+            throw new Exception("n or p should not be negavtive");
+        }
+        else{
+            return (long)Math.pow(n,p);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, p;
+        while(sc.hasNextInt()){
+            n = sc.nextInt();
+            p = sc.nextInt();
+            try{
+                System.out.println(MyCalculator.power(n,p));
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+
