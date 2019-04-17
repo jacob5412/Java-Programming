c0d3fbb3b516591c902747f7937daad8f449bdfd
Jacob <kapsjacob@gmail.com>
2019-04-17 17:39:02 +0530

    cleaned up code

--- LoginLogout/LoginLogoutDB/src/java/FirstServlet.java
+++ LoginLogout/LoginLogoutDB/src/java/FirstServlet.java
@@ -8,61 +8,51 @@
  *
  * @author jacobjohn
  */
-import java.io.IOException;  
-import java.io.PrintWriter;  
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-  
-import javax.servlet.RequestDispatcher;  
-import javax.servlet.ServletException;  
-import javax.servlet.http.HttpServlet;  
-import javax.servlet.http.HttpServletRequest;  
-import javax.servlet.http.HttpServletResponse;  
-  
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
 
-class Login{
-public static boolean validate(String name,String pass){  
-boolean status=false;  
-try{  
-Class.forName("com.mysql.jdbc.Driver");
-Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/JavaJDBC?autoReconnect=true&useSSL=false","root","MyNewPass");  
-      
-PreparedStatement ps=con.prepareStatement(  
-"select * from userreg where name=? and pass=?");  
-ps.setString(1,name);  
-ps.setString(2,pass);  
-      
-ResultSet rs=ps.executeQuery();  
-status=rs.next();  
-          
-}catch(Exception e){System.out.println(e);}  
-return status;  
-}  
-}  
+class Login {
+    public static boolean validate(String name, String pass) {
+        boolean status = false;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/JavaJDBC?autoReconnect=true&useSSL=false", "root", "MyNewPass");
 
-public class FirstServlet extends HttpServlet {  
-public void doPost(HttpServletRequest request, HttpServletResponse response)  
-        throws ServletException, IOException {  
-  
-    response.setContentType("text/html");  
-    PrintWriter out = response.getWriter();  
-          
-    String n=request.getParameter("username");  
-    String p=request.getParameter("userpass");  
-          
-    if(Login.validate(n, p)){
-        RequestDispatcher rd=request.getRequestDispatcher("servlet2"); 
-        rd.forward(request,response);  
-    }  
-    else{  
-        out.print("Sorry username or password error");  
-        RequestDispatcher rd=request.getRequestDispatcher("index.html");  
-        rd.include(request,response);  
-    }  
-          
-    out.close();  
-    }  
-} 
-  
+            PreparedStatement ps = con.prepareStatement("select * from userreg where name=? and pass=?");
+            ps.setString(1, name);
+            ps.setString(2, pass);
+
+            ResultSet rs = ps.executeQuery();
+            status = rs.next();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return status;
+    }
+}
+
+public class FirstServlet extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String n = request.getParameter("username");
+        String p = request.getParameter("userpass");
+
+        if (Login.validate(n, p)) {
+            RequestDispatcher rd = request.getRequestDispatcher("servlet2");
+            rd.forward(request, response);
+        } else {
+            out.print("Sorry username or password error");
+            RequestDispatcher rd = request.getRequestDispatcher("index.html");
+            rd.include(request, response);
+        }
+
+        out.close();
+    }
+}
--- LoginLogout/LoginLogoutDB/src/java/WelcomeServlet.java
+++ LoginLogout/LoginLogoutDB/src/java/WelcomeServlet.java
@@ -1,10 +1,6 @@
-import java.io.IOException;  
-import java.io.PrintWriter;  
-  
-import javax.servlet.ServletException;  
-import javax.servlet.http.HttpServlet;  
-import javax.servlet.http.HttpServletRequest;  
-import javax.servlet.http.HttpServletResponse;  
+import java.io.*;
+import javax.servlet.*;  
+import javax.servlet.http.*;
   
 public class WelcomeServlet extends HttpServlet {  
 public void doPost(HttpServletRequest request, HttpServletResponse response)  
